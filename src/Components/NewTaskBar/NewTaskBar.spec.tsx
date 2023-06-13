4ffe41b6aa2de61a25e2afdba85a50bf2eeedb19
venturions <alex.eduardo10@hotmail.com>
2023-06-13 18:03:56 -0300

    testes no componente de NewTaskBar

--- src/Components/NewTaskBar/NewTaskBar.spec.tsx
+++ src/Components/NewTaskBar/NewTaskBar.spec.tsx
@@ -1,10 +1,46 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { NewTaskBar } from './NewTaskBar'
 
-it('should render a input and a button', () => {
-  const { getByRole } = render(<NewTaskBar addTask={() => null} />)
-  const input = getByRole('textbox')
-  const button = getByRole('button')
-  expect(input).toBeInTheDocument()
-  expect(button).toBeInTheDocument()
+describe('NewTaskBar', () => {
+  it('should render correctly', () => {
+    render(<NewTaskBar addTask={jest.fn()} />)
+
+    const inputElement = screen.getByPlaceholderText('Adicione uma nova tarefa')
+    const buttonElement = screen.getByText('Criar')
+
+    expect(inputElement).toBeInTheDocument()
+    expect(buttonElement).toBeInTheDocument()
+  })
+
+  it('should update the task description correctly', () => {
+    render(<NewTaskBar addTask={jest.fn()} />)
+
+    const inputElement = screen.getByPlaceholderText(
+      'Adicione uma nova tarefa',
+    ) as HTMLInputElement
+
+    fireEvent.change(inputElement, { target: { value: 'Nova tarefa' } })
+
+    expect(inputElement.value).toBe('Nova tarefa')
+  })
+
+  it('should call the addTask function when clicking the button', () => {
+    const mockAddTask = jest.fn()
+    render(<NewTaskBar addTask={mockAddTask} />)
+
+    const inputElement = screen.getByPlaceholderText(
+      'Adicione uma nova tarefa',
+    ) as HTMLInputElement
+    const buttonElement = screen.getByText('Criar')
+
+    fireEvent.change(inputElement, { target: { value: 'Nova tarefa' } })
+    fireEvent.click(buttonElement)
+
+    expect(mockAddTask).toHaveBeenCalledWith({
+      title: 'Nova tarefa',
+      completed: false,
+    })
+
+    expect(inputElement.value).toBe('')
+  })
 })
