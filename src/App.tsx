417720e67092d997dc424c94c25556c40487cee4
venturions <alex.eduardo10@hotmail.com>
2023-06-14 00:23:15 -0300

    validação tarefa já existente e adição de uma notificação de erro

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { enqueueSnackbar } from 'notistack'
 import styles from './App.module.css'
 import { Header } from './Components/Header/Header'
 import { NewTaskBar } from './Components/NewTaskBar/NewTaskBar'
@@ -16,7 +17,15 @@ function App() {
   const [tasks, setTasks] = useState<TaskProps[]>([])
 
   function addTask(newTask: TaskProps) {
-    setTasks([...tasks, newTask])
+    const taskAlreadyExists = tasks.some((item) => item.title === newTask.title)
+
+    if (!taskAlreadyExists) {
+      return setTasks([...tasks, newTask])
+    }
+
+    enqueueSnackbar('Já existe uma tarefa com essa descrição.', {
+      variant: 'error',
+    })
   }
 
   function deleteTask(taskToDelete: string) {
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { SnackbarProvider } from 'notistack'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <SnackbarProvider
+      maxSnack={3}
+      autoHideDuration={3000}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+    >
+      <App />
+    </SnackbarProvider>
+  </React.StrictMode>,
+)
